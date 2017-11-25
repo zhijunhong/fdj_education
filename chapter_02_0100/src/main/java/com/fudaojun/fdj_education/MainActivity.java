e790eb431fecb3b8e7f4a0c9ba90dd142dc12639
ZhijunHong <hongzhijun@wisezone.org>
2017-11-25 19:13:41 +0800

    Activity生命周期和Service生命周期

--- chapter_02_0100/src/main/java/com/fudaojun/fdj_education/MainActivity.java
+++ chapter_02_0100/src/main/java/com/fudaojun/fdj_education/MainActivity.java
@@ -1,14 +1,53 @@
 package com.fudaojun.fdj_education;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    private static final String TAG = "MainActivity";
+
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Log.e(TAG, "start onCreate~~~");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.e(TAG, "start onStart~~~");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.e(TAG, "start onRestart~~~");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.e(TAG, "start onResume~~~");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.e(TAG, "start onPause~~~");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.e(TAG, "start onStop~~~");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.e(TAG, "start onDestroy~~~");
     }
 }
